5d1bfe1b2fc8bec4c6922258d6dd9910e8dc866d
zammsara <jsavalos@uamv.edu.ni>
2024-06-25 10:04:10 -0600

    trabajo final

---
+++ clinicaDental/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string.h>
+#include "funciones.cpp" // Para la gestión de citas
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    principal();
+    return 0;
+}
